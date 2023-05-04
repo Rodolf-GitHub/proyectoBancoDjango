a155301a0571d54bfb4bd87d068df6d258a567e2
Rodolf-GitHub <rodolfogroero2@gmail.com>
2023-05-04 11:48:03 -0400

    trabajando en las url

--- proyectoBanco/proyectoBanco/urls.py
+++ proyectoBanco/proyectoBanco/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from app1.views import hola_mundo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('hola',hola_mundo),
 ]
